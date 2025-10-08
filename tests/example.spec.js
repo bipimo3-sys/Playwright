7f839a86a5d2f280ee15ce65fc12050268a30f78
bipimoh3.sys <bipimoh3@gmail.com>
2025-10-08 17:01:47 +0530

    message

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,21 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/Resume/index.html");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("Resume - Bipi");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("login", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/Resume/index.html");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.fill("#name", "admin");
+  await page.fill("#password", "admin123");
+  page.once("dialog", async (dialog) => {
+    console.log("alert message", dialog.message());
+    expect(dialog.message()).toBe("Login success");
+    await dialog.accept();
+  });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole("button", { name: "Login" }).click();
 });
