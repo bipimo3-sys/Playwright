aa096aa59c39904428d573e6b3a7fbca4f0eefcb
bipimoh3.sys <bipimoh3@gmail.com>
2025-10-09 12:16:07 +0530

    committed

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://127.0.0.1:5500/Resume/index.html");
+  await page.goto("http://127.0.0.1:5500/App01/index.html");
 
   await expect(page).toHaveTitle("Resume - Bipi");
 });
 
 test("login", async ({ page }) => {
-  await page.goto("http://127.0.0.1:5500/Resume/index.html");
+  await page.goto("http://127.0.0.1:5500/App01/index.html");
 
   await page.fill("#name", "admin");
   await page.fill("#password", "admin123");
@@ -20,7 +20,7 @@ test("login", async ({ page }) => {
 });
 
 test.only("add task", async ({ page }) => {
-  await page.goto("http://127.0.0.1:5500/Resume/index.html");
+  await page.goto("http://127.0.0.1:5500/App01/index.html");
 
   await page.fill("#task", "Task 1");
   await page.getByRole("button", { name: "Add New Task" }).click();
