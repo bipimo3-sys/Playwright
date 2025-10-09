9a7d7dffa288791133c9e7709b49fe29e330977c
bipimoh3.sys <bipimoh3@gmail.com>
2025-10-09 11:37:41 +0530

    message

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,6 +16,13 @@ test("login", async ({ page }) => {
     expect(dialog.message()).toBe("Login success");
     await dialog.accept();
   });
-
   await page.getByRole("button", { name: "Login" }).click();
+});
+
+test.only("add task", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/Resume/index.html");
+
+  await page.fill("#task", "Task 1");
+  await page.getByRole("button", { name: "Add New Task" }).click();
+  //await page.press("#task", "Enter");
 });
