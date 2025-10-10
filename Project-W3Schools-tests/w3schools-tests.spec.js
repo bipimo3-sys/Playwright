2b9ccab3531b8be9a9e87503f96a827be3687572
bipimo3-sys <bipimo3@gmail.com>
2025-10-10 14:17:28 +0530

    committed

--- Project-W3Schools-tests/w3schools-tests.spec.js
+++ Project-W3Schools-tests/w3schools-tests.spec.js
@@ -1,5 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({ quiet: true });
 import { test, expect } from "@playwright/test";
+const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
 
 test.only("login", async ({ page }, testInfo) => {
   try {
@@ -10,9 +11,6 @@ test.only("login", async ({ page }, testInfo) => {
 
     const dropdown = page.locator("#tnb-login-dropdown-loginForm").first();
     await expect(dropdown).toBeVisible({ timeout: 500 });
-
-    //const googleBtn = page.locator(".social-button.google");
-    //await googleBtn.click();
 
     const [popup] = await Promise.all([
       page.waitForEvent("popup"), // wait for the new window
@@ -25,18 +23,14 @@ test.only("login", async ({ page }, testInfo) => {
     const password = process.env.GOOGLE_PASSWORD;
     await popup.locator("input[type='email']").fill(email);
     await popup.locator("button:has-text('Next')").click();
-    //await expect(popup.locator("input[type='password']")).toBeVisible({ timeout: 1000 });
     await popup.waitForLoadState("networkidle");
     await popup.waitForSelector("input[type='password']");
     await popup.locator("input[type='password']").fill(password);
     await popup.locator("button:has-text('Next')").click();
 
-    await expect(page).toHaveURL("https://pathfinder.w3schools.com/", { timeout: 20000 });
+    await expect(page).toHaveURL("https://pathfinder.w3schools.com/", { timeout: 40000 });
 
-    //await page.keyboard.press("Escape");
-    //await page.pause(); //will only work on headed mode
-    await popup.waitForLoadState("networkidle");
-    await popup.waitForSelector('section[role="dialog"] button[aria-label="Close"]');
+    await page.waitForSelector('section[role="dialog"] button[aria-label="Close"]');
 
     const modalCloseBtn = page.locator('section[role="dialog"] button[aria-label="Close"]');
 
@@ -44,14 +38,17 @@ test.only("login", async ({ page }, testInfo) => {
       // wait max 3s
       await modalCloseBtn.click();
     }
-    //await popup.waitForSelector("p", { hasText: "Bipi Moh" });
-    const userTxt = await page.locator("p", { hasText: "Bipi Moh" }); //only expect creates screenshots on failure
+
+    //const userTxt = await page.locator("p", { hasText: "Bipi Moh" }); //only expect creates screenshots on failure
+    const userTxt = await page.locator("p", { hasText: "Bipi Mo" }); //only expect creates screenshots on failure
 
     await expect(userTxt).toBeVisible({ timeout: 10000 });
+
+    await page.screenshot({ path: `screenshots/page-${timestamp}.png`, fullPage: true }); //explicit screenshot
   } catch (err) {
     // Take a screenshot safely, if page is still open
     console.log("error=========>", err);
-    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+
     if (!page.isClosed()) {
       await testInfo.attach(`screenshot-${timestamp}`, {
         body: await page.screenshot({ fullPage: true }),
