bbbe99887fe31047876ee2e38c8fd3efa70c0a45
bipimo3-sys <bipimo3@gmail.com>
2025-10-10 13:13:53 +0530

    committed

--- Project-W3Schools-tests/w3schools-tests.spec.js
+++ Project-W3Schools-tests/w3schools-tests.spec.js
@@ -1,36 +1,60 @@
 require("dotenv").config();
 import { test, expect } from "@playwright/test";
 
-test.only("login", async ({ page }) => {
-  await page.goto("https://www.w3schools.com");
-  await page.waitForLoadState("networkidle");
-  const loginButton = page.locator("#tnb-login-btn");
-  await loginButton.click();
+test.only("login", async ({ page }, testInfo) => {
+  try {
+    await page.goto("https://www.w3schools.com");
+    await page.waitForLoadState("networkidle");
+    const loginButton = page.locator("#tnb-login-btn");
+    await loginButton.click();
 
-  const dropdown = page.locator("#tnb-login-dropdown-loginForm").first();
-  await expect(dropdown).toBeVisible({ timeout: 500 });
+    const dropdown = page.locator("#tnb-login-dropdown-loginForm").first();
+    await expect(dropdown).toBeVisible({ timeout: 500 });
 
-  //const googleBtn = page.locator(".social-button.google");
-  //await googleBtn.click();
+    //const googleBtn = page.locator(".social-button.google");
+    //await googleBtn.click();
 
-  const [popup] = await Promise.all([
-    page.waitForEvent("popup"), // wait for the new window
-    page.locator(".social-button.google").click(), // click that opens it
-  ]);
+    const [popup] = await Promise.all([
+      page.waitForEvent("popup"), // wait for the new window
+      page.locator(".social-button.google").click(), // click that opens it
+    ]);
 
-  await popup.waitForLoadState();
+    await popup.waitForLoadState();
 
-  const email = process.env.GOOGLE_EMAIL;
-  const password = process.env.GOOGLE_PASSWORD;
-  await popup.locator("input[type='email']").fill(email);
-  await popup.locator("button:has-text('Next')").click();
-  //await expect(popup.locator("input[type='password']")).toBeVisible({ timeout: 1000 });
-  await popup.waitForLoadState("networkidle");
-  await popup.waitForSelector("input[type='password']");
-  await popup.locator("input[type='password']").fill(password);
-  await popup.locator("button:has-text('Next')").click();
+    const email = process.env.GOOGLE_EMAIL;
+    const password = process.env.GOOGLE_PASSWORD;
+    await popup.locator("input[type='email']").fill(email);
+    await popup.locator("button:has-text('Next')").click();
+    //await expect(popup.locator("input[type='password']")).toBeVisible({ timeout: 1000 });
+    await popup.waitForLoadState("networkidle");
+    await popup.waitForSelector("input[type='password']");
+    await popup.locator("input[type='password']").fill(password);
+    await popup.locator("button:has-text('Next')").click();
 
-  await expect(page).toHaveURL("https://pathfinder.w3schools.com/", { timeout: 50000 });
-  const userTxt = page.locator("p", { hasText: "Bipi Mo" });
-  await expect(userTxt).toBeVisible({ timeout: 50000 });
+    await expect(page).toHaveURL("https://pathfinder.w3schools.com/", { timeout: 20000 });
+
+    //await page.keyboard.press("Escape");
+    //await page.pause(); //will only work on headed mode
+
+    const modalCloseBtn = page.locator('section[role="dialog"] button[aria-label="Close"]');
+
+    if (await modalCloseBtn.isVisible({ timeout: 3000 })) {
+      // wait max 3s
+      await modalCloseBtn.click();
+    }
+    const userTxt = await page.locator("p", { hasText: "Bipi Moh" }); //only expect creates screenshots on failure
+
+    await expect(userTxt).toBeVisible({ timeout: 10000 });
+  } catch (err) {
+    // Take a screenshot safely, if page is still open
+    console.log("error=========>", err);
+    const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+    if (!page.isClosed()) {
+      await testInfo.attach(`screenshot-${timestamp}`, {
+        body: await page.screenshot({ fullPage: true }),
+        contentType: "image/png",
+      });
+    }
+    throw err; // rethrow so the test fails
+  }
 });
--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './Project-W3Schools-tests',
+  testDir: "./Project-W3Schools-tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,23 +24,26 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters 
   reporter: 'html',*/
-  reporter: [['html', { outputFolder: 'playwright-report'}]],
+  reporter: [["html", { outputFolder: "playwright-report" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    headless: true, // run tests in headed mode
+    viewport: { width: 1280, height: 800 },
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
-
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -49,7 +52,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
@@ -79,4 +82,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
