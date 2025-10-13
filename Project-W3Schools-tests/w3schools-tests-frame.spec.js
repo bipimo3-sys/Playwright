427f894296c2d571f246aeb973de16b092c8fac4
bipimoh3.sys <bipimoh3@gmail.com>
2025-10-13 10:08:46 +0530

    committed

---
+++ Project-W3Schools-tests/w3schools-tests-frame.spec.js
@@ -0,0 +1,35 @@
+require("dotenv").config({ quiet: true });
+import { test, expect } from "@playwright/test";
+const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+
+test.only("iframe", async ({ page }, testInfo) => {
+  try {
+    await page.goto(
+      "https://www.w3schools.com/html/tryit.asp?filename=tryhtml_default"
+    );
+    await page.waitForLoadState("networkidle");
+
+    const htmlInput = `<p><b><i>Hello world!</i></b></p>`;
+    const textarea = page.locator("#textareaCode");
+    await textarea.fill(htmlInput);
+
+    await page.locator("#runbtn").click();
+
+    const frame = page.frameLocator("#iframeResult");
+    await expect(frame.locator("body")).toContainText("Hello world!");
+
+    await page.screenshot({
+      path: `screenshots/page-${timestamp}.png`,
+      fullPage: true,
+    }); //explicit screenshot
+  } catch (err) {
+
+    if (!page.isClosed()) {
+      await testInfo.attach(`screenshot-${timestamp}`, {
+        body: await page.screenshot({ fullPage: true }),
+        contentType: "image/png",
+      });
+    }
+    throw err; // rethrow so the test fails
+  }
+});
