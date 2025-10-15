c9576becedf74b84d285977e2cfa443deccc41e5
bipimoh3.sys <bipimoh3@gmail.com>
2025-10-15 13:50:10 +0530

    committed

--- playwright.config.js
+++ playwright.config.js
@@ -11,6 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 /**
  * @see https://playwright.dev/docs/test-configuration
+ * 
  */
 export default defineConfig({
   testDir: "./ProjectTestsuite",
